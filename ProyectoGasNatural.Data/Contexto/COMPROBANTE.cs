d6fc77581a3d0c379344529ddeb62de88b2b5f4f
JohanStivenMartinez <jmartine207@uniminuto.edu.co>
2020-11-20 12:44:59 -0500

    Actualizacion del sistema V1.0.0

---
+++ ProyectoGasNatural.Data/Contexto/COMPROBANTE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoGasNatural.Data.Contexto
+{
+    public class COMPROBANTE
+    {
+        public int IdComprobante { get; set; }
+        public double Monto { get; set; }
+        public double Iva { get; set; }
+        public BadImageFormatException FirmaTecnico { get; set; }  ///             Duda en BadImagenFormatException  ..°°.. lml
+        public BadImageFormatException FirmaUsuario { get; set; }
+        public int IdServicio { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaImpresion { get; set; }
+
+    }
+}
---
+++ ProyectoGasNatural.Data/Contexto/ETAPAS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoGasNatural.Data.Contexto
+{
+    public class ETAPAS
+    {
+        public int IdEtapa { get; set; }
+        public string Etapa { get; set; }
+    }
+}
