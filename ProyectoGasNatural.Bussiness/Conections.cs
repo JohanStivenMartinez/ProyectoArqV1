e538adcaa1b69cc933839bb061078fd48b6e5d77
JohanStivenMartinez <jmartine207@uniminuto.edu.co>
2020-11-24 17:24:32 -0500

    Conexion a la base de datos - Azure

---
+++ ProyectoGasNatural.Bussiness/Conections.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Configuration;
+
+namespace ProyectoGasNatural.Bussiness
+{
+    class Conections
+    {
+        
+        private SqlConnection conexion = new SqlConnection(ConfigurationManager.AppSettings["StringConect"]);
+        private string respuesta = String.Empty;
+        
+
+        public void abrirConexion()
+        {
+            try
+            {
+                conexion.Open();
+                Console.WriteLine("Conectado");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+                Console.WriteLine("Cerrada Conexion");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void revisarConsulta()
+        {
+            
+            try
+            {
+                abrirConexion();
+                cerrarConexion();
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+        
+    }
+}
