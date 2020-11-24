735aa2334afde1e425ad0495834f3da5410aea06
Daniel Pregonero <dpregone@uniminuto.edu.co>
2020-11-24 17:33:23 -0500

    Creacion de agregar usuario

---
+++ ProyectoArqV1/Views/Js/agregarUsuario.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const router = Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname + '/Desktop/agregarUsuario.html'));
+});
+
+router.post('/', (req, res) => {
+
+     console.log(req.body);
+     res.send('resivido');
+   
+});
+
+module.exports = router;
