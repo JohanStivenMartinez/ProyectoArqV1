d207ba4a6d8ae9937519d19eca5a04cd05400abe
SergioCuervoL <36489747+SergioCuervoL@users.noreply.github.com>
2020-11-26 00:08:06 -0500

    Nuevo formato para la capa de negocio

--- GasNatural.Servicios/Areas/HelpPage/Views/Help/DisplayTemplates/TextSample.vbhtml
+++
@@ -1,6 +0,0 @@
-@Imports GasNatural.Servicios.Areas.HelpPage
-@ModelType TextSample
-
-<pre class="wrapped">
-@Model.Text
-</pre>
---
+++ GasNatural/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GasNatural.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
